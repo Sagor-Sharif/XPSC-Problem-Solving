23178829d4f0651e2f84546937764a2f4cf26dad
Sagor Sharif <126094782+Sagor-Sharif@users.noreply.github.com>
2024-11-27 23:54:19 +0600

    Add files via upload

--- Week3/Week 3  Topicwise  Two Pointers & Sliding Window/Number of Equal.cpp
+++ Week3/Week 3  Topicwise  Two Pointers & Sliding Window/Number of Equal.cpp
@@ -6,36 +6,28 @@ int main()
     cin.tie(nullptr);
     int n,m;
     cin>>n>>m;
-    int a[n],b[m];
+    map<long long,long long> A;
     for(int i=0; i< n; i++)
     {
-        cin>>a[i];
+        int a;
+        cin>>a;
+        A[a]++;
     }
-    map<int,int> B;
+    map<long long,long long> B;
     for(int i=0; i< m; i++)
     {
-        cin>>b[i];
-        B[b[i]]++;
+        int b;
+        cin>>b;
+        B[b]++;
     }
-    int last_j=0,total_c=0;
-    int last_c=0;
-    for(int i=0; i< m; i++)
+    long long last_j=0,total_c=0;
+
+    for(auto[x,y]:B)
     {
-        int c=0;
-        for(int j=last_j ; j<n; j++)
-        {
-            if(b[i]==a[j])
-            {
-                c++;
-            }
-            else
-            {
-                last_j=j;
-            }
-        }
-        total_c+=(B[b[i]]*c);
+        long long c = A[x];
+        total_c+=(y*c);
     }
     cout<<total_c<<endl;
-return 0;
+    return 0;
 }
 
