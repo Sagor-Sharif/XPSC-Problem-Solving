b52c7b4660d23c97c13176b2441a4057ee1ec37e
Sagor Sharif <126094782+Sagor-Sharif@users.noreply.github.com>
2024-12-20 00:50:16 +0600

    Add files via upload

--- Week5/Special Characters.cpp
+++ Week5/Special Characters.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+
+        if(n<2 || n%2 !=0)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            int loop = n/2;
+            cout<<"YES"<<endl;
+            for(int i=0;i<loop;i++)
+            {
+                char c = 'A'+i;
+                cout<<c<<c<<c;
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
 
